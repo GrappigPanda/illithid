657a754aa79523aac49ba246495b9c3d9ae3213b
Ian Lee Clark <ian@ianleeclark.com>
2020-05-20 16:55:32 +0200

    Updating ex_doc: 0.14.4 -> 0.15.0

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Illithid.MixProject do
       {:jason, "~> 1.1"},
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.14.4", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.15.0", only: :dev, runtime: false}
     ]
   end
 
